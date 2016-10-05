20a0d7c3bc513eed1c0befec2b54aad027004ecd
Sean <sniealu@gmail.com>
2016-10-05 02:12:35 +0000

    test

--- bgl/mini_example2.cpp
+++ bgl/mini_example2.cpp
@@ -11,14 +11,26 @@ namespace detail {
 	    const GraphT& g_;
 		public:
 		    explicit vertex_writer(const GraphT& g) : g_(g) {}
-			template <class VertexDescriptorT>
-			void operator() (std::ostream& out, const VertexDescriptorT& d) const {
+			template <class vertex_descriptor>
+			void operator() (std::ostream& out, const vertex_descriptor& d) const {
 			    out << " [label=\""
 				    << boost::get(boost::vertex_bundle, g_)[d]
 					<< "\"]";
 			}
 	}; // vertex_writer
 } //namespace detail
+
+template <class GraphT>
+void print(const GraphT& g) {
+    typedef typename boost::graph_traits<boost::SampleGraph::Graph>::vertex_iterator vert_it_t;
+	vert_it_t it, end;
+	boost::tie(it,end) = boost::vertices(g);
+	typedef boost::graph_traits<boost::SampleGraph::Graph>::vertex_descriptor desc_t;
+	for(; it!=end; ++it) {
+	    desc_t desc = *it;
+		std::cout<<boost::get(boost::vertex_bundle,g)[desc];
+	}
+}
 
 
 std::ostream& operator<<(std::ostream& out, const boost::SampleGraph::Graph& g) {
@@ -37,6 +49,7 @@ int main() {
 
   int cost =  boost::find_flow_cost(g);
   assert(cost == 29);
+  print(g);
   std::cout<<g; 
   return 0;
 }
